4e8062710d8cb129784c895b5bef66c2dc64129d
Chitrakshi Gosain <chitrakshigosain@gmail.com>
2023-05-27 08:02:24 +1000

    better solution for tax_free

--- Medium/TaxFree/tax_free.py
+++ Medium/TaxFree/tax_free.py
@@ -1,11 +1,19 @@
-TAX = 1.07
+TAX = 0.07
 
-TAXED, TAXLESS = [], []
+prices = [float(price) for price in input().split(',')]
 
-for price in input().split(','):
-    if float(price) < 20:
-        TAXED.append(float(price))
-    else:
-        TAXLESS.append(float(price))
+TAXED = [price for price in prices if price < 20]
 
-print(round(sum(TAXED) * TAX + sum(TAXLESS), 2))
+print(round(sum(TAXED) * TAX + sum(prices) , 2))
+
+# TAX = 1.07
+
+# TAXED, TAXLESS = [], []
+
+# for price in input().split(','):
+#     if float(price) < 20:
+#         TAXED.append(float(price))
+#     else:
+#         TAXLESS.append(float(price))
+
+# print(round(sum(TAXED) * TAX + sum(TAXLESS), 2))
